29c3529b3af1e09378a61a74a121731886dd04fa
Hans Achterbahn <hans.achterbahn@posteo.de>
2021-09-06 21:56:20 +0200

    Automatisches Anlegen von Backup-Kategorien definiert: daily / weekly / monthly

--- keepass-backup.sh
+++ keepass-backup.sh
@@ -1,16 +1,32 @@
 #!/usr/bin/env zsh
 
 # User Einstellungen
-FOLDER=/home/mario/Dropbox/keepass          # Ordner, in dem die Datei liegt
-FILE=mh-pw.kdbx                             # Datei, die zum Backup vorgesehen ist
-BACKUP_SUBFOLDER=Backup                     # Unterordner, in dem das Backup abgelegt werden soll
-DATE="$(date +"%Y-%m-%d") $(date +"%T")"    # Erstellen des Zeitstempels für den Backup Dateinamen
+FOLDER=/home/mario/Dropbox/keepass      # Ordner, in dem die Datei liegt
+FILE=mh-pw.kdbx                         # Datei, die zum Backup vorgesehen ist
+BACKUP_SUBFOLDER=Backup                 # Unterordner, in dem das Backup abgelegt werden soll
+DATE="$(date +"%F")_$(date +"%T")"      # Format des Zeitstempels für den Backup-Dateinamen
+
+
+# Standardmäßig -> Backuptyp "Täglich" (daily-backup)
+TYPE="daily"
+
+# Falls Montag ist -> Backuptyp "Wöchentlich" (weekly-backup)
+if [[ $(date +"%u") == "1" ]]
+then
+  TYPE="weekly"
+fi
+
+# Falls 1. des Monats ist -> Backuptyp "Monatlich" (monthly-backup)
+if [[ $(date +"%d") == "01" ]]
+then
+  TYPE="monthly"
+fi
 
 
 # Erstellen aller vollständigen Pfade
-BACKUP_FOLDER="$FOLDER/$BACKUP_SUBFOLDER"                       # Pfad zum Backupordner
-FROM="$FOLDER/$FILE"                                            # Pfad zur Originaldatei
-TO="$BACKUP_FOLDER/$DATE $BACKUP_SUBFOLDER Autostart - $FILE"   # Pfad zur Backupdatei
+BACKUP_FOLDER="$FOLDER/$BACKUP_SUBFOLDER"      # Pfad zum Backupordner
+FROM="$FOLDER/$FILE"                           # Pfad zur Originaldatei
+TO="$BACKUP_FOLDER/$DATE-$TYPE-backup:$FILE"   # Pfad zur Backupdatei
 
 
 # Prüfen, ob der Backupunterordner bereits angelegt ist und falls nicht, ggf. erstellen
