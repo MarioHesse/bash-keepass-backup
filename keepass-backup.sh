3501b78fcbea7db9bf63a42d893c347e0dcb3534
Hans Achterbahn <hans.achterbahn@posteo.de>
2021-09-06 20:47:30 +0200

    Skript leicht optimiert

--- keepass-backup.sh
+++ keepass-backup.sh
@@ -1,37 +1,26 @@
-#!/bin/bash
-
-# Start des Backups anzeigen
-date
-echo "  --- Start Backup ---"
-
+#!/usr/bin/env zsh
 
 # User Einstellungen
-FOLDER=/home/mario/Dropbox/keepass              # Ordner, in dem die Datei liegt
-FILE=mh-pw.kdbx                                 # Datei, die zum Backup vorgesehen ist
-BACKUP_SUBFOLDER=Backup                         # Unterordner, in den das Backup abgelegt werden soll
-DATE="$(date +"%Y-%m-%d") $(date +"%T")"        # Erstellen des Zeitstempels für den Backup Dateinamen
+FOLDER=/home/mario/Dropbox/keepass          # Ordner, in dem die Datei liegt
+FILE=mh-pw.kdbx                             # Datei, die zum Backup vorgesehen ist
+BACKUP_SUBFOLDER=Backup                     # Unterordner, in dem das Backup abgelegt werden soll
+DATE="$(date +"%Y-%m-%d") $(date +"%T")"    # Erstellen des Zeitstempels für den Backup Dateinamen
 
 
 # Erstellen aller vollständigen Pfade
-BACKUP_FOLDER="$FOLDER/$BACKUP_SUBFOLDER"           # Pfad zum Backupordner
-FROM="$FOLDER/$FILE"                                # Pfad zur Originaldatei
-TO="$BACKUP_FOLDER/$DATE $BACKUP_SUBFOLDER $FILE"   # Pfad zur Backupdatei
+BACKUP_FOLDER="$FOLDER/$BACKUP_SUBFOLDER"                       # Pfad zum Backupordner
+FROM="$FOLDER/$FILE"                                            # Pfad zur Originaldatei
+TO="$BACKUP_FOLDER/$DATE $BACKUP_SUBFOLDER Autostart - $FILE"   # Pfad zur Backupdatei
 
 
-# Prüfen, ob der Backupunterordner bereits angelegt ist und ggf. erstellen
-if [[ -d "$BACKUP_FOLDER" ]]
+# Prüfen, ob der Backupunterordner bereits angelegt ist und falls nicht, ggf. erstellen
+if ! [[ -d "$BACKUP_FOLDER" ]]
 then
-    echo "  Backup folder '$BACKUP_FOLDER' - OK!"
-else
-    mkdir "$BACKUP_FOLDER/"
-	echo "  Backup folder '$BACKUP_FOLDER' created!"
+  mkdir "$BACKUP_FOLDER/"
 fi
 
+# Backup anlegen (Kopieren der Datei → Statusausgabe wird in Variable geschrieben)
+COPY_OUTPUT=$(cp -v "$FROM" "$TO")
 
-# Backup anlegen
-COPY_OUTPUT=$(cp -v "$FROM" "$TO")      # Kopieren der Datei → Status Ausgabe wird in Variable geschrieben
-echo "  $COPY_OUTPUT"                   # Ausgabe des Kopier-Status mit Einrückung
-
-
-# Ende des Backups anzeigen
-echo "  --- End Backup ---"
+# Ausgabe des Kopierstatus mit Datum in einem Log-File
+echo "$DATE: $COPY_OUTPUT" >> "$BACKUP_FOLDER/$BACKUP_SUBFOLDER.log"
