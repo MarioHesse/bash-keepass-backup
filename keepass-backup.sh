09e611a4a8794474c16f483cd703d8e97206a010
Hans Achterbahn <hans.achterbahn@posteo.de>
2022-04-05 22:40:13 +0200

    yearly-backup eingeführt | Unterscheidung von daily, weekly, monthly und yearly verbessert

--- keepass-backup.sh
+++ keepass-backup.sh
@@ -4,29 +4,42 @@
 FOLDER=/home/mario/Dropbox/keepass      # Ordner, in dem die Datei liegt
 FILE=mh-pw.kdbx                         # Datei, die zum Backup vorgesehen ist
 BACKUP_SUBFOLDER=Backup                 # Unterordner, in dem das Backup abgelegt werden soll
-DATE="$(date +"%F")_$(date +"%H%M%S")"  # Format des Zeitstempels für den Backup-Dateinamen
+DATE="$(date +"%F_W%W_%H%M%S")"         # Format des Zeitstempels für den Backup-Dateinamen
 
 
-# Standardmäßig -> Backuptyp "Täglich" (daily-backup)
+# Erstellen aller vollständigen Pfade
+BACKUP_FOLDER="$FOLDER/$BACKUP_SUBFOLDER"           # Pfad zum Backupordner
+FROM="$FOLDER/$FILE"                                # Pfad zur Originaldatei
+
+# Daily
+#   Standardmäßig wird ein tägliches Backup erstellt (daily-backup)
 TYPE="daily"
 
-# Falls Montag ist -> Backuptyp "Wöchentlich" (weekly-backup)
-if [[ $(date +"%u") == "1" ]]
+# Weekly
+#   Falls noch keine Datei mit dem aktuellen Jahr, der aktuellen Woche und dem Tag
+#   "weekly" existiert, wird ein weekly-backup angelegt
+if ! [[ "$(ls -1 ${BACKUP_FOLDER}/$(date +"%Y")*$(date +"W%W")*weekly*$FILE | wc -l)" > "0" ]]
 then
   TYPE="weekly"
 fi
 
-# Falls 1. des Monats ist -> Backuptyp "Monatlich" (monthly-backup)
-if [[ $(date +"%d") == "01" ]]
+# Monthly
+#   Falls noch keine Datei mit dem aktuellen Jahr, dem aktuellen Monat und dem Tag
+#   "monthly" existiert, wird ein monthly-backup angelegt
+if ! [[ "$(ls -1 ${BACKUP_FOLDER}/$(date +"%Y-%m")*monthly*$FILE | wc -l)" > "0" ]]
 then
   TYPE="monthly"
 fi
 
+# Yearly
+#   Falls noch keine Datei mit dem aktuellen Jahr und dem Tag "yearly" existiert,
+#   wird ein yearly-backup angelegt
+if ! [[ "$(ls -1 ${BACKUP_FOLDER}/$(date +"%Y")*yearly*$FILE | wc -l)" > "0" ]]
+then
+  TYPE="yearly"
+fi
 
-# Erstellen aller vollständigen Pfade
-BACKUP_FOLDER="$FOLDER/$BACKUP_SUBFOLDER"      # Pfad zum Backupordner
-FROM="$FOLDER/$FILE"                           # Pfad zur Originaldatei
-TO="$BACKUP_FOLDER/${DATE}_${TYPE}-backup_$FILE"   # Pfad zur Backupdatei
+TO="$BACKUP_FOLDER/${DATE}_${TYPE}-backup_$FILE"    # Pfad zur Backupdatei
 
 
 # Prüfen, ob der Backupunterordner bereits angelegt ist und falls nicht, ggf. erstellen
